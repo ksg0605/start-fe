201de5bd5f9783417cbe9db33744fabeb4f118bf
sunkyu Kim <yyh2337@naver.com>
2021-03-11 19:19:50 +0900

    week2

---
+++ 01/app.js
@@ -0,0 +1,34 @@
+console.log('app.js');
+
+var log = function(str) {
+  console.log(str);
+}
+
+// log('Hello');
+
+// var image = {
+//   width: 100,
+//   height: 100,
+//   name: '1.png'
+// };
+
+// var image1 = [1, 2, 3, 4, 5, 6, 'a', {}];
+
+// var data = null;
+// var data1 = '';
+// var data2;
+
+// console.log(data, data1, data2);
+
+// console.log(1 + 2);
+// console.log('a' + 'b');
+// var count = 1;
+// var number = '2';
+
+//console.log(count + number);
+// console.log('' == 0);
+
+console.log(typeof 1);
+console.log(typeof []);
+console.log(typeof '');
+console.log(typeof {});
