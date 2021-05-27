973d8c640a4c20b873c7f48f62837c6f243d7d1b
sunkyu Kim <yyh2337@naver.com>
2021-05-27 20:55:10 +0900

    add settings

---
+++ 11/src/index.js
@@ -0,0 +1,5 @@
+import "./todos.css";
+
+const $inputForm = document.querySelector("#input-form");
+const $input = document.querySelector("#input");
+const $result = document.querySelector("#result");
